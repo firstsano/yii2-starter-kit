43b644da0834a73da6da44e5111946bce36b977f
Koltunov Alexander <riky@vgg.ru>
2017-08-09 18:59:25 +0300

    Change logging to file logging

--- common/config/base.php
+++ common/config/base.php
@@ -67,17 +67,21 @@ $config = [
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
-                'db'=>[
-                    'class' => 'yii\log\DbTarget',
+                'file' => [
+                    'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
-                    'except'=>['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
-                    'prefix'=>function () {
-                        $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
-                        return sprintf('[%s][%s]', Yii::$app->id, $url);
-                    },
-                    'logVars'=>[],
-                    'logTable'=>'{{%system_log}}'
-                ]
+                ],
+//                'db'=>[
+//                    'class' => 'yii\log\DbTarget',
+//                    'levels' => ['error', 'warning'],
+//                    'except'=>['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
+//                    'prefix'=>function () {
+//                        $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
+//                        return sprintf('[%s][%s]', Yii::$app->id, $url);
+//                    },
+//                    'logVars'=>[],
+//                    'logTable'=>'{{%system_log}}'
+//                ]
             ],
         ],
 
